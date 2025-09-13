f638c34132f838af0ae371e7e5815d3814bc6fa5
Ancuyou <nguyenngocthaibaodetox@gmail.com>
2025-09-13 21:41:42 +0700

    Giao dien home page

--- src/main/java/it/ute/QAUTE/controller/HomeController.java
+++ src/main/java/it/ute/QAUTE/controller/HomeController.java
@@ -2,12 +2,11 @@ package it.ute.QAUTE.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
     public String home() {
-        return "home";
+        return "pages/home";
     }
 }
