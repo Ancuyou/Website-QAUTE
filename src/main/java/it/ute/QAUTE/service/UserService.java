fcad5e349726eb45ab3da32917369d3aa311bc62
nguyenit24 <nguyenhuynh.463459@gmail.com>
2025-10-06 10:41:58 +0700

    fix page

--- src/main/java/it/ute/QAUTE/service/UserService.java
+++ src/main/java/it/ute/QAUTE/service/UserService.java
@@ -1,8 +1,6 @@
 package it.ute.QAUTE.service;
 
-import it.ute.QAUTE.entity.Account;
-import it.ute.QAUTE.entity.User;
-import it.ute.QAUTE.repository.AccountRepository;
+
 import it.ute.QAUTE.repository.UserReponsitory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
